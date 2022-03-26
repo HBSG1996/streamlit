57e161de10cce0519fb76007317d3114dec86bac
HECTOR <46928863+HBSG1996@users.noreply.github.com>
2022-03-26 17:48:28 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 17:28:08 2022
+
+@author: PC
+"""
+
+import streamlit as st
+
+st.write("""
+         # My first APP
+         This is my first app
+         """)
