75f6de582616973496049d34054e7db8c5f06747
HECTOR <46928863+HBSG1996@users.noreply.github.com>
2022-03-26 18:10:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,45 @@ Created on Sat Mar 26 17:28:08 2022
 """
 
 import streamlit as st
+import requests
+import json
+from requests.exceptions import RequestException
+import time
+import pandas as pd
 
-st.write("""
-         # My first APP
-         This is my first app!
-         """)
-st.info("It's ok!")
+headers = {
+    'Host': 'api.fund.eastmoney.com',
+    'Referer': 'http://fundf10.eastmoney.com/jjjz_161725.html',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+}
+
+def get_response(url,params):
+    try:
+        response = requests.get(url,params=params,headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException as e:
+        print('err: %s' % e)
+
+
+
+def main():
+	t = time.time()
+	nowTime = lambda:int(round(t * 1000))
+	params = {
+		'callback': 'jQuery183015901735224567126_1549080604934',
+		'fundCode': '161725',
+		'pageIndex': '2',
+		'pageSize': '0',
+		'_': nowTime()
+	}
+	
+	text = get_response('http://api.fund.eastmoney.com/f10/lsjz',params=params)
+	res = text.replace('jQuery183015901735224567126_1549080604934(','')
+	tar_str = res[:-1]
+	get_json = json.loads(tar_str)
+	return get_json#print(get_json)
+
+data = pd.DataFrame(main()['Data']['LSJZList'])
+st.dataframe(data)
